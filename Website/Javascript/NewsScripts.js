8f3e479945517c05a21df8ca36e7783e308bf77d
W-Dominic <Dom0_o@protonmail.com>
2020-12-07 16:45:37 -0500

    almost done, added sidebar

--- Website/Javascript/NewsScripts.js
+++ Website/Javascript/NewsScripts.js
@@ -1,2 +1,15 @@
-let side = document.querySelector(".sidebar");
-let sideButton =document.querySelector(".menu") 
+var menu = document.querySelector(".collapsible");
+var content = document.querySelector(".content");
+var vis = false;
+
+function collapse(){
+	if (vis == false){
+	content.style.display = "block";
+	vis = true;
+	}
+	else if (vis == true){
+	content.style.display = "none";
+	vis = false;
+	}
+}
+menu.onclick = collapse;
