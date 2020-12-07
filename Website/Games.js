bd34c6dfade3c42e5bcaf0bb9ed9a0b2cde65f0a
shreya6302 <75502600+shreya6302@users.noreply.github.com>
2020-12-06 20:37:13 -0500

    Add files via upload

---
+++ Website/Games.js
@@ -0,0 +1,35 @@
+
+
+
+function button1() {
+  var str = "Play Valorant";
+  var result = str.link("https://playvalorant.com/en-us/");
+  document.getElementById("button1").innerHTML = result;
+}
+
+function button2() {
+  var str = "Play Apex Legends";
+  var result = str.link("https://www.ea.com/games/apex-legends/play-now-for-free");
+  document.getElementById("button2").innerHTML = result;
+}
+
+function button3() {
+  var str = "Buy Demon's Souls";
+  var result = str.link("https://www.playstation.com/en-us/games/demons-souls/");
+  document.getElementById("button3").innerHTML = result;
+}
+
+function button4() {
+  var str = "Buy The Last of Us 2";
+  var result = str.link("https://www.playstation.com/en-us/games/the-last-of-us-part-ii-ps4/");
+  document.getElementById("button4").innerHTML = result;
+}
+
+function button5() {
+  var str = "Buy Animal Crossing: New Horizons";
+  var result = str.link("https://www.gamestop.com/collection/animal-crossing-new-horizons?utm_kxconfid=teby7kvd7&gclid=EAIaIQobChMIzYS45Nq67QIVTPvICh1DvAbwEAAYASAAEgIKTvD_BwE&gclsrc=aw.ds");
+  document.getElementById("button5").innerHTML = result;
+}
+
+
+
