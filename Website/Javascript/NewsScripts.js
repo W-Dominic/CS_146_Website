73334a7221dab16de3ec3fc7c70365d4c422bfa9
Dom W <media.Dom0_o@protonmail.com>
2020-12-07 21:30:50 -0500

    ctf

--- Website/Javascript/NewsScripts.js
+++ Website/Javascript/NewsScripts.js
@@ -13,3 +13,26 @@ function collapse(){
 	}
 }
 menu.onclick = collapse;
+
+var but = document.querySelector(".submit");
+function order66(){
+	var comment = document.querySelector("#Comment").value;	
+	if (comment === "secret"){
+		alert("The password is: #abc123");
+	}
+	else if (comment === "#abc123"){
+		alert("The next password must be deciphered: 99 115 49 52 54");
+	}
+	else if (comment === "cs146"){
+		alert("Nice!, The next password is hidden in one of the images on this site");
+	}
+	else if (comment === "commonsenseisnotsocommon"){
+		alert("Congrats!, You win :D");
+	}
+	else {
+		alert("Your comment was submitted"); 
+	}
+	location = location;
+}
+
+but.onclick=order66;
